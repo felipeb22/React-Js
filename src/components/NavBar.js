a9a7543954d15252fb12c5632ef70bab866b83b2
Felipe Bahl <felipebahl22@outlook.com>
2022-03-08 11:22:53 -0300

    desafios 1 y 2 bien cargados

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+function NavBar(){
+    return <header>
+        <h1>Photo Tienda</h1>
+        <nav>
+            <a href="#">Camaras</a>
+            <a href="#">Accesorios</a>
+            <a href="#">Objetivos</a>
+            <a href="#">Usados</a>
+        </nav>
+    </header>
+}
+
+export default NavBar
